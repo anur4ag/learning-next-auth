7e741014825db00cf9b3f0f1882a9d4cbecedb48
Anurag Sharma <71564387+anur4ag@users.noreply.github.com>
2024-01-26 00:57:38 +0530

    Update auth.ts and schema.prisma files

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import authConfig from "./auth.config";
+import authConfig from "@/auth.config";
+import { getUserById } from "@/data/user";
 import { db } from "@/lib/db";
 
 export const {
@@ -9,6 +10,31 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      console.log({ sessionToken: token, session });
+      return session;
+    },
+    async jwt({ token }) {
+      console.log("JWT TOKEN", token);
+      if (!token.sub) {
+        return token;
+      }
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
