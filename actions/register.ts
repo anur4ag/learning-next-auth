30e20f436d8ed62045707fea0aec9d2f93203278
Anurag Sharma <71564387+anur4ag@users.noreply.github.com>
2024-01-25 15:33:24 +0530

    login and register form UI+server action setup

---
+++ actions/register.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import * as z from "zod";
+import { RegisterSchema } from "@/schemas";
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedField = RegisterSchema.safeParse(values);
+  if (!validatedField.success) {
+    return { error: "Invalid Field" };
+  }
+  return { success: "Email sent" };
+};
---
+++ app/auth/register/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import LoginForm from "@/components/auth/LoginForm";
+import RegisterForm from "@/components/auth/RegisterForm";
+
+const RegisterPage = () => {
+  return <RegisterForm />;
+};
+
+export default RegisterPage;
