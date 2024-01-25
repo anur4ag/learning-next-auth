8ea3836991443f514098b9aa68a633f65732c7d0
Anurag Sharma <71564387+anur4ag@users.noreply.github.com>
2024-01-25 20:48:25 +0530

    Add user registration functionality and connected neon pg db

--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,34 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedField = RegisterSchema.safeParse(values);
   if (!validatedField.success) {
     return { error: "Invalid Field" };
   }
-  return { success: "Email sent" };
+  console.log(validatedField.data);
+  const { email, password, name } = validatedField.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification email
+
+  return { success: "User created successfully" };
 };
